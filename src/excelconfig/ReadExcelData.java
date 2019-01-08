cc454f7d01894dab4fba48df95a34ec59303f7ea
Abhishek Khatod <abhishek.khatod@yash.com>
2019-01-08 23:34:22 +0530

    Committing Excel Utility AK

---
+++ src/excelconfig/ReadExcelData.java
@@ -0,0 +1,76 @@
+package excelconfig;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcelData {
+
+	/*public static void main(String[] args) throws IOException {
+		
+		String FilePath = "C:\\Users\\abhishek.khatod\\Documents\\eclipse_workspace\\yash.TCOEFrameWork\\src\\main\\java\\Resources";
+		String FileName = "ReadData.xlsx";
+		String SheetName = "FirstSheet";
+		System.out.println(ReadExcelData.ReadExcel(FilePath, FileName, SheetName));
+	}*/
+	
+	public static Map<String, String> ReadExcel(String FilePath, String FileName, String SheetName) throws IOException {
+		
+		//Map for storing the data from excel
+		HashMap<String, String> ExcelData = new HashMap<String, String>();
+		
+		File file = new File(FilePath + "\\" + FileName);
+		FileInputStream inputStream = new FileInputStream(file);
+		Workbook wb = null;
+
+		//To check the extension of file and create object accordingly
+		if (FileName.substring(FileName.indexOf(".")).equalsIgnoreCase(".xlsx")) {
+			wb = new XSSFWorkbook(inputStream);
+		} else if(FileName.substring(FileName.indexOf(".")).equalsIgnoreCase(".xls")){
+			wb = new HSSFWorkbook(inputStream);
+		}
+
+		// To get the sheet where data has to be read from
+		Sheet sheet = wb.getSheet(SheetName);
+
+		for (int i = 0;i<sheet.getLastRowNum(); i++) {
+
+			// To get the row
+			Row row = sheet.getRow(i);
+			
+			//System.out.println("row : "+row.getCell(0).getStringCellValue());
+			
+			for(int j=1;j<row.getLastCellNum();j++) {
+				
+				ExcelData.put(row.getCell(0).getStringCellValue(), row.getCell(j).getStringCellValue());
+				
+				//loop for skipping blank cells
+				if(row.getCell(j).getStringCellValue() != null) {
+					
+					System.out.println(row.getCell(0).getStringCellValue()+" --> "+row.getCell(j).toString());
+				}else {
+					continue;
+				}
+			}
+			System.out.println();
+		}
+		
+		inputStream.close();
+		
+		System.out.println("Data is successfully (Abhishek) read from the sheet..!!");
+		
+		for(Map.Entry<String, String> Map_Data : ExcelData.entrySet()) {
+			
+			System.out.println(Map_Data.getKey()+" : "+Map_Data.getValue());
+		}
+		
+		return ExcelData;
+	}
+}
---
+++ src/excelconfig/WriteExcelData.java
@@ -0,0 +1,58 @@
+package excelconfig;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class WriteExcelData {
+
+	public static void WriteExcel(String FilePath, String FileName, String SheetName) throws IOException {
+		
+		File file = new File(FilePath+ "\\" +FileName);
+		
+		FileInputStream InputStream = new FileInputStream(file);
+		Workbook wb = null;
+		
+		if (FileName.substring(FileName.indexOf(".")).equalsIgnoreCase(".xlsx")) {
+			
+			wb = new XSSFWorkbook(InputStream);
+		} else if(FileName.substring(FileName.indexOf(".")).equalsIgnoreCase(".xls")){
+			
+			wb = new HSSFWorkbook(InputStream);
+		}
+		
+		Sheet sheet = wb.getSheet(SheetName);
+		
+		Row row = sheet.getRow(0);
+		
+		int countOfRow = sheet.getLastRowNum() - sheet.getFirstRowNum();
+		
+		Row newRow = sheet.createRow(countOfRow+1);
+		
+		for(int i=0;i<row.getLastCellNum();i++) {
+			
+			Cell cell = newRow.createCell(i);
+			
+			cell.setCellValue("Abhi"+i);
+		}
+		
+		InputStream.close();
+		
+		FileOutputStream outputStream = new FileOutputStream(file);
+		
+		wb.write(outputStream);
+		
+		outputStream.close();
+		
+		System.out.println("Data is successfully written on the sheet..!!");
+		
+	}
+}
